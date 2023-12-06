154f6ec6dc6076293f065e07f1f3e7bc2b2bba69
NITHINKR06 <146012853+NITHINKR06@users.noreply.github.com>
2023-12-06 18:35:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import StarrySky from './component/SkyStar'
+
+function App() {
+  return (
+    <div className="App">
+     <StarrySky/>
+      <center>
+        <h1>Welcome</h1>
+      </center>
+    </div>
+  );
+}
+
+export default App;
